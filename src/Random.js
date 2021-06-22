e055b6e9a02f172b9ef3bc336baab0ee8bfa354d
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-06-22 14:44:34 -0700

    Started setting up App.js & bones for components

---
+++ src/Random.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class random extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default random;
