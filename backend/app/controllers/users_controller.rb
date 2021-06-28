12cae9db9aa2fda4df35f9883bc51e5c45bb5c6c
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-06-28 15:51:31 -0700

    removed updating for user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :destroy]
+    before_action :set_user, only: [:show, :destroy]
 
     def index
       @users = User.all
@@ -14,14 +14,6 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         render json: @user, status: :created, location: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
-
-    def update
-      if @user.update(user_params)
-        render json: @user
       else
         render json: @user.errors, status: :unprocessable_entity
       end
