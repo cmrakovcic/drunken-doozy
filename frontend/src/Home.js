a331b3e3ab280dac6628cb2f335eafb2a839463c
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-06-29 14:21:20 -0700

    Styled up home page along w instructions on using app

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,12 +6,13 @@ class Home extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
+                <header className="App-header"><br></br>
+                <br></br>
                     <img src={"https://pngimg.com/uploads/beer/beer_PNG2330.png"} className="App-logo" alt="logo" />
-                    <h1>Congratulations on creating an account!</h1>
-                    <p>To get started click on the "Beers" tab in the nav bar to view a list of beers with their ingredients along with the option to add that beer to your favorited beers.</p>
-                    <p>Click on the "Favorited Beers" tab in the nav bar to view your favorited beers.</p>
-                    <p>Click on the "Random Beer" tab in the nav bar to genrate a random beer along with the option to add it to your favorited beers.</p>
+                    <h2>Congratulations on creating an account!</h2>
+                    <p>To get started click on the "Beers" tab in the nav bar to view a list of beers with their ingredients, along with the option to add that beer to your favorited beers list.</p>
+                    <p>Click on the "Favorited Beers" tab in the nav bar to view your favorited beers list.</p>
+                    <p>Click on the "Random Beer" tab in the nav bar to genrate a random beer, along with the option to add it to your favorited beers list.</p>
                 </header>
             </div>
         );
