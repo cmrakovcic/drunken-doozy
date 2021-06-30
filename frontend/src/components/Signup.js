cef8cd2f0ae3106de9a8f05db32aaccee0ba2c5e
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-06-30 13:50:58 -0700

    still working on Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+import { addUser, addUserBE } from '../reducers/actions';
+// import { withRouter } from "react-router";
 
 class Signup extends Component {
+
+    state = {
+        user: ""
+    }
+
+    handleChange = (evt) => {
+        this.setState({
+          [evt.target.email]: evt.target.value
+        })
+      }
+    
+    handleSubmit = (evt) => {
+        evt.preventDefault()
+        // this.props.addUser(this.state)
+        // this.props.addUserBE(this.state.user)
+        this.props.history.push(`/home`)
+      }
+
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <form>
-                        <h2>Signup</h2>
-                        <div>
-                        <input type="text" name="email" placeholder="Email" />
-                        <label htmlFor="email"></label>
-                        </div>
-                        <div>
-                        <input type="password" name="password" placeholder="Password" />
-                        <label htmlFor="password"></label>
-                        </div>
-                        <input type="submit" value="submit" />
-                    </form>
-                </header>
+            <div>
+                <h1>Signup</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="user">Email</label>
+                    <input
+                        onChange={this.handleChange}
+                        type="text"
+                        email="user"
+                        // value={this.state.user}
+                        autoComplete="off"
+                        id="user"
+                    />
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            onChange={this.handleChange}
+                            type="text"
+                            password="password"
+                            // value={this.state.user}
+                            autoComplete="off"
+                            id="user"
+                        />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
+
         );
     }
 }
 
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         users: () => { dispatch(addUserBE) }
+//     }
+// }
+
 export default Signup;
+// export default connect(null, { addUserBE, addUser, mapDispatchToProps })(Signup);
