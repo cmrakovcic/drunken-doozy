a3ca3cc3ca4f40781f7dc06c43a8498ea646f978
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-07-02 13:26:24 -0700

    set up jwt

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
     before_action :set_user, only: [:show, :destroy]
+
+    def profile
+      render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
 
     def index
       @users = User.all
@@ -10,12 +15,22 @@ class UsersController < ApplicationController
       render json: @user
     end
 
+    # def create
+    #   @user = User.new(user_params)
+    #   if @user.save
+    #     render json: @user, status: :created, location: @user
+    #   else
+    #     render json: @user.errors, status: :unprocessable_entity
+    #   end
+    # end
+
     def create
-      @user = User.new(user_params)
-      if @user.save
-        render json: @user, status: :created, location: @user
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token({ user_id: @user.id })
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
       else
-        render json: @user.errors, status: :unprocessable_entity
+        render json: { error: 'failed to create user' }, status: :not_acceptable
       end
     end
 
