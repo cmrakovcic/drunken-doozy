a246a95a4624397f222edb61267433629abd23d9
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-07-06 16:47:54 -0700

    started fetching list of beers

--- frontend/src/components/Beers.js
+++ frontend/src/components/Beers.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Footer from './Footer';
-import { getAllBeers } from '../reducers/actions'
+// import { getAllBeers } from '../reducers/actions'
 
 class Beers extends Component {
+    state = {
+        beers: []
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <h2>List of Beers</h2>
-                    <li beers={getAllBeers}></li>
+                    <li>{this.state.beers}</li>
+                    {/* <li>{getAllBeers()}</li> */}
                 </header>
                 < Footer/>
             </div>
